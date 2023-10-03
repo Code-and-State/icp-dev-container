5d0b713a7a5df6fc410a76ccd7706c543f8b400b
Sebastien Thuillier <sebastien.thuillier38@gmail.com>
2023-10-03 13:30:01 +0100

    Fix: modify test for Ubuntu 22.04

--- test/icp-motoko/test.sh
+++ test/icp-motoko/test.sh
@@ -3,7 +3,7 @@ cd $(dirname "$0")
 source test-utils.sh
 
 # Template specific tests
-check "distro" lsb_release -c
+check "ubuntu-version" [ "$(lsb_release -rs)" = "22.04" ]
 check "dfx command existence" command -v dfx
 
 # Report result
